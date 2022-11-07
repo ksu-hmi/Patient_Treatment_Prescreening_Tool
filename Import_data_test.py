ea119788806715d88fbb1a42876d76bdb83028ed
apatinga <apatinga@students.kennesaw.edu>
2022-11-06 21:50:56 -0500

    testing codes

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,16 +1,4 @@
-import pandas as pd
-import requests
-import io
-    
-# Downloading the csv file from your GitHub account
+import os 
+os.system("curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
 
-url = "https://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/blob/main/Python_Project_Data.xlsx?raw=true" # Make sure the url is the raw version of the file on GitHub
-download = requests.get(https://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/blob/main/Python_Project_Data.xlsx?raw=true).content
-
-# Reading the downloaded content and turning it into a pandas dataframe
-
-df = pd.read_xlsx(io.StringIO(download.decode('utf-8')))
-
-# Printing out the first 5 rows of the dataframe
-
-print (df.head())
+print(os)
