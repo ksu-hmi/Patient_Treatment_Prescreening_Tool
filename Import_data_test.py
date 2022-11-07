854b320bc2437e5f84b7ff01958e12bf30816893
apatinga <apatinga@students.kennesaw.edu>
2022-11-07 15:52:57 -0500

    updating file

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,4 +1,3 @@
 import os 
 os.system("curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
 
-print(os)
