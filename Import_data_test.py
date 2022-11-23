609e6c0d7255330f6f104c11008c72d7be7eb7a3
apatinga <apatinga@students.kennesaw.edu>
2022-11-23 12:44:35 -0500

    trying to import dataset

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,3 +1,13 @@
 import os 
-os.system("curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
+
+cmd = "curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv"
+
+os.system(cmd)
+
+diagonsis_file = open('Python_Project_Data_Diagnosis.csv', 'r')
+diagnosis = diagonsis_file.readlines()
+
+print (diagnosis)
+
+
 
