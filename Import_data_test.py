9e41e0f771d2d7c50d02bc8b41268b42199d1c55
apatinga <apatinga@students.kennesaw.edu>
2022-12-03 21:51:43 -0500

    trying pandas

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,5 +1,5 @@
 import os 
-os.system("curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
+os.system("curl https://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
 
 open("Python_Project_Data_Diagnosis.csv","r")
 
