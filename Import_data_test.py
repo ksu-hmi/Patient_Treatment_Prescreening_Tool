86fe623f1f09c3cd6193a08e7f5164af5d6bd1b1
apatinga <apatinga@students.kennesaw.edu>
2022-12-03 22:02:35 -0500

    trying to import!

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,7 +1,6 @@
 import os 
-os.system("curl https://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
+os.system("curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Medications.csv -o Python_Project_Data_Medications.csv")
 
-
-
+open('Python_Project_Data_Medications.csv')
 
 
