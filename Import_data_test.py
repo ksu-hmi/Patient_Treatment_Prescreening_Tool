3ca0aa55100abb17cdc099e5102e92ec306340a9
apatinga <apatinga@students.kennesaw.edu>
2022-12-04 12:10:13 -0500

    attempted to code import

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,4 +1,4 @@
 import os 
-os.system("curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Medications.csv -o Python_Project_Data_Medications.csv")
 
-
+cmd = "curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv"
+os.system(cmd)
