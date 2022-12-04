9b57bf376e7f17d0f98708d4852faa476027973c
apatinga <apatinga@students.kennesaw.edu>
2022-12-03 21:54:28 -0500

    importing

--- Import_data_test.py
+++ Import_data_test.py
@@ -1,7 +1,6 @@
 import os 
 os.system("curl https://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv")
 
-open("Python_Project_Data_Diagnosis.csv","r")
 
 
 
