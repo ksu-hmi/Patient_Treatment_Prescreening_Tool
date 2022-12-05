5535a29cbe7a05e220e89d9abb2a97481b344975
apatinga <111015227+apatinga@users.noreply.github.com>
2022-12-05 16:22:48 -0500

    Code not needed

--- Import_data_test.py
+++
@@ -1,4 +0,0 @@
-import os 
-
-cmd = "curl http://github.com/ksu-hmi/Patient_Treatment_Prescreening_Tool/raw/main/Python_Project_Data_Diagnosis.csv -o Python_Project_Data_Diagnosis.csv"
-os.system(cmd)
